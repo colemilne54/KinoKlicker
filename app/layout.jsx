559f2deb27bcd57b86156162c78080971abda4b0
Cole Milne <colemilne54@gmail.com>
2023-03-16 00:05:53 -0600

    API initial setup call/ get successful. 31:11

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,19 @@
 import './globals.css'
+import { Montserrat } from "@next/font/google"
+
+const montserrat = Montserrat({
+	weight: ["400", "700"],
+	subsets: ["latin"],
+	variable: "--font-montserrat",
+})
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
+==			</head>
 
-			</head>
-
-			<body>
+			<body className={`${montserrat.className}`}>
 				<nav>
 					<h1>logo</h1>
 					<ul>
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  console.log(res)
+  
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Hello next 13 🔥</h1>
     </main>
   )
 }
+

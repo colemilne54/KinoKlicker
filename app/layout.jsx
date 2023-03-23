3c37008384be26ded4b2ab1082b2385e405d77e9
Cole Milne <colemilne54@gmail.com>
2023-03-23 13:03:20 -0600

    Finishing touches

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,18 +9,9 @@ const montserrat = Montserrat({
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
-			<head></head>
-			<body className={`${montserrat.className} mx-32 my-12`}>
-				<nav>
-					<h1>logo</h1>
-					<ul>
-						<li><a href="/about">About</a></li>
-						<li><a href="#">Sign up</a></li>
-					</ul>
-				</nav>
-				{children}
-			</body>
-		</html>
+	  <html lang="en">
+		<head />
+		<body className={`${montserrat.className} m-32`}>{children}</body>
+	  </html>
 	)
-}
+  }
